b1df36db04bbfba2600e2ca3e38e0a6972ee3c5f
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-05-20 07:35:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+function setup() {
+  createCanvas(480, 240);
+}
+
+function draw() {
+  background(0,191,255);
+  
+
+}
+
+function mousePressed() {
+ 
+//add the push operation
+//print it in the console
+}
+
+function keyPressed() {
+//add the pop operation
+//print it in the console
+}
+
